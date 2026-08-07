72d070bb0733f2825efbfc12dc3be7d6dc069419
Jaime Martret <jaime.martret@gmail.com>
2026-08-07 02:30:13 +0200

    Add dependency transition and event serializers

--- quests/serializers.py
+++ quests/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from quests.models import Quest
+from quests.models import Quest, QuestDependency, QuestEvent
 
 
 class QuestSerializer(serializers.ModelSerializer):
@@ -21,3 +21,32 @@ class QuestSerializer(serializers.ModelSerializer):
             "updated_at",
         )
         read_only_fields = ("id", "project", "state", "created_at", "updated_at")
+
+
+class QuestDependencySerializer(serializers.ModelSerializer):
+    prerequisite_id = serializers.IntegerField()
+
+    class Meta:
+        model = QuestDependency
+        fields = ("id", "dependent", "prerequisite_id", "created_at")
+        read_only_fields = ("id", "dependent", "created_at")
+
+
+class QuestTransitionSerializer(serializers.Serializer):
+    target_state = serializers.ChoiceField(choices=Quest.State.choices)
+
+
+class QuestEventSerializer(serializers.ModelSerializer):
+    actor_id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = QuestEvent
+        fields = (
+            "id",
+            "quest_id_snapshot",
+            "event_type",
+            "actor_id",
+            "data",
+            "created_at",
+        )
+        read_only_fields = fields
