2f020be5094c48d1d0471b8bfddd80740e750cd3
Jaime Martret <jaime.martret@gmail.com>
2026-08-07 02:00:06 +0200

    Add initial project migration

---
+++ projects/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="Project",
+            fields=[
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=200)),
+                ("description", models.TextField(blank=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="ProjectMembership",
+            fields=[
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("role", models.CharField(choices=[("OWNER", "Owner"), ("REVIEWER", "Reviewer"), ("CONTRIBUTOR", "Contributor")], max_length=16)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("project", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="memberships", to="projects.project")),
+                ("user", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="project_memberships", to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddConstraint(
+            model_name="projectmembership",
+            constraint=models.UniqueConstraint(fields=("project", "user"), name="uniq_project_membership_user"),
+        ),
+    ]
