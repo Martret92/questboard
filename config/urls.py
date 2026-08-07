c642851929c552f483633ad59af6a0657a96396b
Jaime Martret <jaime.martret@gmail.com>
2026-08-07 03:27:36 +0200

    Expose OpenAPI schema and docs

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from config.views import health
 
 urlpatterns = [
+    path("health/", health, name="health"),
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/", include("projects.urls")),
     path("api/", include("quests.urls")),
 ]
